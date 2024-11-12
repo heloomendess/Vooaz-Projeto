aa6ada7b7867c74e438b283f06aaf87571eed873
Grazielly Cavalcante <grazioliveira2705@gmail.com>
2024-11-11 21:57:15 -0300

    Add files via upload

---
+++ Banco de Dados Vooaz/guias.js
@@ -0,0 +1,137 @@
+db.guias.insertMany([
+    {
+      "_id": ObjectId("635b1b17a23c4327e2a5adf1"),
+      "nome": "Carlos Ferreira",
+      "especialidade": "Guia de Turismo Cultural",
+      "localidade": "São Paulo",
+      "idiomas": ["Português", "Inglês"],
+      "avaliacao": 4.8,
+      "email": "carlos.ferreira@email.com",
+      "contato": "+5511991234567",
+      "tipo": "Autônomo",
+      "especialidadeDeficiencia": "Deficiência Visual"
+    },
+    {
+      "_id": ObjectId("635b1b17a23c4327e2a5adf2"),
+      "nome": "Ana Souza",
+      "especialidade": "Guia de Turismo Natural",
+      "localidade": "Litoral Norte",
+      "idiomas": ["Português", "Espanhol"],
+      "avaliacao": 4.6,
+      "email": "ana.souza@email.com",
+      "contato": "+5511987654321",
+      "tipo": "Vinculado a empresa",
+      "especialidadeDeficiencia": "Deficiência Auditiva"
+    },
+    {
+      "_id": ObjectId("635b1b17a23c4327e2a5adf3"),
+      "nome": "Beatriz Santos",
+      "especialidade": "Guia de Acessibilidade",
+      "localidade": "São Paulo",
+      "idiomas": ["Português"],
+      "avaliacao": 4.9,
+      "email": "beatriz.santos@email.com",
+      "contato": "+5511998765432",
+      "tipo": "Autônomo",
+      "especialidadeDeficiencia": "Ambas"
+    },
+    {
+      "nome": "Alaide Carvalho Ribeiro",
+      "especialidade": "Guia de Turismo Natural",
+      "localidade": "São Paulo",
+      "idiomas": ["Português", "Inglês"],
+      "avaliacao": 5.0,
+      "email": "alaideribe409@gmail.com",
+      "contato": "+5511909458763",
+      "tipo": "Autônomo",
+      "especialidadeDeficiencia": "Deficiência Visual"
+    },
+    {
+      "nome": "Marina Guimarães",
+      "especialidade": "Guia de acessibilidade",
+      "localidade": "Litoral Norte",
+      "idiomas": ["Português"],
+      "avaliacao": 4.7,
+      "email": "mariguima2040@gmail.com",
+      "contato": "+5511970825639",
+      "tipo": "Vinculado a empresa",
+      "especialidadeDeficiencia": "Deficiência Auditiva"
+    },
+    {
+      "nome": "Priscila Furlan",
+      "especialidade": "Guia de Turismo Cultural",
+      "localidade": "Litoral Norte",
+      "idiomas": ["Português", "Inglês"],
+      "avaliacao": 4.3,
+      "email": "prifurlan@gmail.com",
+      "contato": "+5511914896572",
+      "tipo": "Vinculado a empresa",
+      "especialidadeDeficiencia": "Ambos"
+    },
+    {
+      "nome": "Henrique Costa",
+      "especialidade": "Guia de Turismo Natural",
+      "localidade": "Litoral Norte",
+      "idiomas": ["Português", "Espanhol", "Inglês"],
+      "avaliacao": 4.6,
+      "email": "henriqeucosta3098@gmail.com",
+      "contato": "+5511940587632",
+      "tipo": "Vinculado a empresa",
+      "especialidadeDeficiencia": "Deficiência Auditiva"
+    },
+
+    {
+
+      "nome": "Jucelino da Silva",
+      "especialidade": "Guia de acessibilidade ",
+      "localidade": "São Paulo",
+      "idiomas": ["Português"],
+      "avaliacao": 4.9,
+      "email": "julelinosilva@gmail.com",
+      "contato": "+5511978459682",
+      "tipo": "Autônomo",
+      "especialidadeDeficiencia": "Deficiência Visual"
+    },
+    {
+  
+      "nome": "Guilherme Ferreira da Silva",
+      "especialidade": "Guia de Turismo Cultural",
+      "localidade": "Litoral Norte",
+      "idiomas": ["Português"],
+      "avaliacao": 3.9,
+      "email": "guilhermefereira2028@gmail.com",
+      "contato": "+5511946859752",
+      "tipo": "Autônomo",
+      "especialidadeDeficiencia": "Ambos"
+    },
+    {
+  
+      "nome": "Cristina Ribeiro Paiva",
+      "especialidade": "Guia de acessibilidade",
+      "localidade": "São Paulo",
+      "idiomas": ["Português", "Espanhol", "Inglês"],
+      "avaliacao":5.0,
+      "email": "crisribeiro3039@gmail.com",
+      "contato": "+5511930179846",
+      "tipo": "Vinculado a empresa",
+      "especialidadeDeficiencia": "Deficiência Auditiva"
+    },
+    {
+    
+      "nome": "Felipe Barbosa Queiroz",
+      "especialidade": "Guia de Turismo Natural",
+      "localidade": "Litoral Norte",
+      "idiomas": ["Português", "Espanhol"],
+      "avaliacao": 4.5,
+      "email": "felipebarbosa@gmail.com",
+      "contato": "+5511945697823",
+      "tipo": "Vinculado a empresa",
+      "especialidadeDeficiencia": "Ambos"
+    },
+
+
+
+
+
+  ]);
+  
