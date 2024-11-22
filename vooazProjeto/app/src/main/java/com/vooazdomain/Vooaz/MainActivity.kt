cf422082bcb6e857c604469751a51fd8e4acbd59
Aluno@SPROAK1010 <iurysven82@gmail.com>
2024-11-22 13:50:34 -0300

    telas-pagamentos-telas-planos-font-poppins

--- vooazProjeto/app/src/main/java/com/vooazdomain/Vooaz/MainActivity.kt
+++ vooazProjeto/app/src/main/java/com/vooazdomain/Vooaz/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.vooazdomain.Vooaz.telas.InputScreen
 import com.vooazdomain.Vooaz.telas.LoginScreen
+import com.vooazdomain.Vooaz.telas.Pagamento
+import com.vooazdomain.Vooaz.telas.PagamentoFeito
 import com.vooazdomain.Vooaz.ui.theme.VooazTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import com.vooazdomain.Vooaz.telas.PlanScreenSuggestion
+import com.vooazdomain.Vooaz.telas.Planos
 import com.vooazdomain.Vooaz.telas.ResgisterAccount
 import com.vooazdomain.Vooaz.telas.resetpassword.ForgotPassword
 import com.vooazdomain.Vooaz.telas.resetpassword.ForgotPasswordPin
@@ -36,7 +39,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             VooazTheme() {
                 val navController = rememberNavController()
-                NavHost(navController= navController , startDestination = "PlanScreenSuggestion") {
+                NavHost(navController= navController , startDestination = "Planos") {
 
                     composable("PlanScreenSuggestion") {
                         PlanScreenSuggestion(navController)
@@ -59,6 +62,15 @@ class MainActivity : ComponentActivity() {
                     }
                     composable("InputScreen"){
                         InputScreen(navController)
+                    }
+                    composable("Planos") {
+                        Planos(navController)
+                    }
+                    composable("Pagamento") {
+                        Pagamento(navController)
+                    }
+                    composable("PagamentoFeito") {
+                        PagamentoFeito(navController)
                     }
                 }
 
