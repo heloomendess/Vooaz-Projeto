417a2a923947ab73416e81b8e372a6768f502476
Grazielly Cavalcante <grazioliveira2705@gmail.com>
2024-11-22 14:22:31 -0300

    Update reservas.js

--- Banco de Dados Vooaz/reservas.js
+++ Banco de Dados Vooaz/reservas.js
@@ -1,27 +1,98 @@
 db.reservas.insertMany([
-    {
-      "usuario_id": "635aecd0e5564331f0e7bb33", 
-      "guia_id": "635b1b17a23c4327e2a5adf2",
-      "destino_id": "635b2f77c71f1c45e5d7ba19",
-      "data_reserva": ISODate("2024-11-15T10:00:00Z"),
-      "status": "Confirmada",
-      "pagamento": "Completo"
+   
+   
+  {
+      "usuario_id": ObjectId('635aecd0e5564331f0e7bb35'),
+      "guia_id":ObjectId('6740af0728680fbb345d4a6e') ,
+      "destino_id": ObjectId('67390bad34736bb489f89ab6'),
+      "plano_id":ObjectId('645aede1f5564331f0e7cc08') ,
+      "data_reserva": new Date("2024-12-20")
+     
     },
-    {
-      "usuario_id": "635aecd0e5564331f0e7bb34",
-      "guia_id": "635b1b17a23c4327e2a5adf3",
-      "destino_id": "635b2f77c71f1c45e5d7ba20",
-      "data_reserva": ISODate("2024-12-20T15:00:00Z"),
-      "status": "Pendente",
-      "pagamento": "Em Aberto"
-    },
-    {
-      "usuario_id": "635aecd0e5564331f0e7bb35",
-      "guia_id": "635b1b17a23c4327e2a5adf4",
-      "destino_id": "635b2f77c71f1c45e5d7ba21",
-      "data_reserva": ISODate("2024-10-25T08:30:00Z"),
-      "status": "Cancelada",
-      "pagamento": "Reembolsado"
-    }
+
+      {
+        "usuario_id": ObjectId('674091ed0f69f25217eba23c'),
+        "guia_id":ObjectId('6740af0728680fbb345d4a6d') ,
+        "destino_id": ObjectId('67390bad34736bb489f89abb'),
+         "plano_id":ObjectId('645aede1f5564331f0e7cc08') ,
+        "data_reserva": new Date("2024-11-24")
+      },
+
+      {
+        "usuario_id": ObjectId('674091ed0f69f25217eba23e'),
+        "guia_id":ObjectId('6740af0728680fbb345d4a6c') ,
+        "destino_id": ObjectId('67390bad34736bb489f89ab4'),
+         "plano_id":ObjectId('645aede1f5564331f0e7cc08') ,
+        "data_reserva": new Date("2024-11-15")
+      },
+{
+        "usuario_id": ObjectId('635aecd0e5564331f0e7bb32'),
+        "guia_id":ObjectId('6740af0728680fbb345d4a69') ,
+        "destino_id": ObjectId('645aedc0f5564331f0e7cc02'),
+         "plano_id":ObjectId('645aede1f5564331f0e7cc07') ,
+        "data_reserva": new Date("2024-10-20")
+
+},
+{
+        "usuario_id": ObjectId('674091ed0f69f25217eba239'),
+        "guia_id":ObjectId('6740af0728680fbb345d4a68') ,
+        "destino_id": ObjectId('645aedc0f5564331f0e7cc04'),
+         "plano_id":ObjectId('645aede1f5564331f0e7cc07') ,
+        "data_reserva": new Date("2024-12-20")
+},
+{
+        "usuario_id": ObjectId('674091ed0f69f25217eba241'),
+        "guia_id":ObjectId('6740af0728680fbb345d4a65') ,
+        "destino_id": ObjectId('645aedc0f5564331f0e7cc03'),
+         "plano_id":ObjectId('645aede1f5564331f0e7cc07') ,
+        "data_reserva": new Date("2024-12-20")
+},
+{
+        "usuario_id": ObjectId('674091ed0f69f25217eba247'),
+        "guia_id":ObjectId('6740af0728680fbb345d4a67') ,
+        "destino_id": ObjectId('67390bad34736bb489f89aa7'),
+         "plano_id":ObjectId('645aede1f5564331f0e7cc07') ,
+        "data_reserva": new Date("2024-12-20")
+},
+{
+       "usuario_id": ObjectId('674091ed0f69f25217eba244'),
+        "guia_id":ObjectId('6740af0728680fbb345d4a6a') ,
+        "destino_id": ObjectId('67390bad34736bb489f89ab3'),
+         "plano_id":ObjectId('645aede1f5564331f0e7cc07') ,
+        "data_reserva": new Date("2024-11-12")
+},
+
+
+{
+  "usuario_id": ObjectId(''),
+  "guia_id":ObjectId('') ,
+  "destino_id": ObjectId(''),
+   "plano_id":ObjectId('645aede1f5564331f0e7cc06') ,
+  "data_reserva": new Date("2024-12-20")
+},
+
+{
+"usuario_id": ObjectId('635aecd0e5564331f0e7bb31'),
+  "guia_id":ObjectId('635b1b17a23c4327e2a5adf1') ,
+  "destino_id": ObjectId('645aedc0f5564331f0e7cc02'),
+   "plano_id":ObjectId('645aede1f5564331f0e7cc06') ,
+  "data_reserva": new Date("2024-12-14")
+},
+{
+"usuario_id": ObjectId('674091ed0f69f25217eba23b'),
+  "guia_id":ObjectId('6740af0728680fbb345d4a69') ,
+  "destino_id": ObjectId('645aedc0f5564331f0e7cc01'),
+   "plano_id":ObjectId('645aede1f5564331f0e7cc06') ,
+  "data_reserva": new Date("2024-12-25")
+},
+
+{
+"usuario_id": ObjectId('674091ed0f69f25217eba242'),
+  "guia_id":ObjectId('635b1b17a23c4327e2a5adf1') ,
+  "destino_id": ObjectId('645aedc0f5564331f0e7cc04'),
+   "plano_id":ObjectId('645aede1f5564331f0e7cc06') ,
+  "data_reserva": new Date("2025-01-20")
+},
+
   ]);
   
