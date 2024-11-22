07ac44172c847f5f7a3605ccb4e8dba378978718
Grazielly Cavalcante <grazioliveira2705@gmail.com>
2024-11-22 11:53:40 -0300

    Add files via upload

---
+++ Banco de Dados Vooaz/planos (1).js
@@ -0,0 +1,215 @@
+
+db.planos.insertMany([
+    {
+      "_id": ObjectId("645aede1f5564331f0e7cc06"),
+      "nome_plano": "Voo Inicial",
+      "mensalidade": 0,
+      "anual": 0,
+      "beneficios": [
+        "Roteiros de viagens",
+        "Visitas em grupo",
+        "Experiências culturais",
+        "Guias de acessibilidade"
+      ],
+     "usuarios": [
+     
+            {
+              "nome": "João Oliveira",
+              "email": "joao.oliveira@email.com",
+              "tipoDeficiencia": "Visual",
+              "dataNascimento": {
+                "$date": "1996-05-15T00:00:00.000Z"
+              }
+            },
+            {
+              "nome": "Mariana Costa",
+              "email": "mariana.costa@email.com",
+              "tipoDeficiencia": "Visual",
+              "dataNascimento": {
+                "$date": "1990-07-22T00:00:00.000Z"
+              }
+            },
+            {
+              "nome": "Gabriel Carvalho de moraes",
+              "email": "carvalhogabriel@gmail.com",
+              "tipoDeficiencia": "Auditiva",
+              "dataNascimento": {
+                "$date": "1998-05-26T00:00:00.000Z"
+              }
+            },
+            {
+              "nome": "Nicolas Bittencort Alencar",
+              "email": "nicolasbitt@gmail.com",
+              "tipoDeficiencia": "Visual",
+              "dataNascimento": {
+                "$date": "2003-03-27T00:00:00.000Z"
+              }
+            },
+            {
+              "nome": "Bruno Lima Ribeiro",
+              "email": "limabruno@gmail.com",
+              "tipoDeficiencia": "Auditiva",
+              "dataNascimento": {
+                "$date": "2002-05-24T00:00:00.000Z"
+              }
+            },
+            {
+              "nome": "Felipe de sousa Carvalho",
+              "email": "felipesousa3098@gmail.com",
+              "tipoDeficiencia": "Visual",
+              "dataNascimento": {
+                "$date": "1999-11-25T00:00:00.000Z"
+              }
+            }
+      ],
+       
+    },
+    {
+      "_id": ObjectId("645aede1f5564331f0e7cc07"),
+      "nome_plano": "Explorador",
+      "mensalidade": 29.99,
+      "anual": 359.88,
+      "beneficios": [
+        "Guias de acessibilidade",
+        "Roteiros de viagens",
+        "Visitas em grupo",
+        "Match com viajantes",
+        "Transporte para locais culturais"
+      ],
+      "usuarios": [
+        {
+          "nome": "Larissa Mendes",
+          "email": "larissa.mendes@email.com",
+          "tipoDeficiencia": "Auditiva",
+          "dataNascimento": {
+            "$date": "1989-11-20T00:00:00.000Z"
+          }
+        },
+        {
+          "nome": "Mariana Rodrigues de Paiva  ",
+          "email": "marianapaiva@gmail.com",
+          "tipoDeficiencia": "Auditiva",
+          "dataNascimento": {
+            "$date": "2003-08-05T00:00:00.000Z"
+          }
+        },
+        {
+          "nome": "Alexandra Felix Araujo",
+          "email": "alearaujo2007@gmail.com",
+          "tipoDeficiencia": "Auditiva",
+          "dataNascimento": {
+            "$date": "2005-10-05T00:00:00.000Z"
+          }
+        },
+        {
+          "nome": "Gisele Massafera de Sousa",
+          "email": "giselemassafera@gmail.com",
+          "tipoDeficiencia": "Auditiva",
+          "dataNascimento": {
+            "$date": "1996-04-08T00:00:00.000Z"
+          }
+        },
+        {
+          "nome": " Hosana Martins Machado",
+          "email": "nanamartins@gmail.com",
+          "tipoDeficiencia": "Visual",
+          "dataNascimento": {
+            "$date": "1997-07-01T00:00:00.000Z"
+          }
+        },
+        {
+          "nome": "Fernanda Lopes de Sousa",
+          "email": "fernandalopes3007@gmail.com",
+          "tipoDeficiencia": "Auditiva",
+          "dataNascimento": {
+            "$date": "2002-03-24T00:00:00.000Z"
+          }
+        }
+      ]
+
+    
+    },
+    {
+      "_id": ObjectId("645aede1f5564331f0e7cc08"),
+      "nome_plano": "Sem Fronteiras",
+      "mensalidade": 149.99,
+      "anual": 1199,
+      "beneficios": [
+        "Intercâmbios culturais",
+        "Dicas internacionais",
+        "Transporte cultural",
+        "Guias especializados"
+      ],
+      
+  "usuarios": [
+    {
+      "nome": "Eduardo Lima",
+      "email": "eduardo.lima@email.com",
+      "tipoDeficiencia": "Visual e Auditiva",
+      "dataNascimento": {
+        "$date": "2002-02-03T00:00:00.000Z"
+      }
+    },
+    {
+      "nome": "Felipe Andrade",
+      "email": "felipe.andrade@email.com",
+      "tipoDeficiencia": "Auditiva",
+      "dataNascimento": {
+        "$date": "1998-09-10T00:00:00.000Z"
+      }
+    },
+    {
+      "nome": "Emanuel Magno Cabral",
+      "email": "emanuelmcabral@gmail.com",
+      "tipoDeficiencia": "Visual",
+      "dataNascimento": {
+        "$date": "2001-07-03T00:00:00.000Z"
+      }
+    },
+    {
+      "nome": "Aline Ribeiro Rodrigues",
+      "email": "lineribeiro02@gmail.com",
+      "tipoDeficiencia": "Visual",
+      "dataNascimento": {
+        "$date": "2004-03-02T00:00:00.000Z"
+      }
+    },
+    {
+      "nome": "João da Silva Carvalho",
+      "email": "joãosilva304@gail.com",
+      "tipoDeficiencia": "Auditiva",
+      "dataNascimento": {
+        "$date": "1997-10-27T00:00:00.000Z"
+      }
+    },
+    {
+      "nome": "Pietro Monteiro Albuquerque",
+      "email": "pietromc2023@gmail.com",
+      "tipoDeficiencia": "Auditiva e Visual",
+      "dataNascimento": {
+        "$date": "1998-03-24T00:00:00.000Z"
+      }
+    },
+    {
+      "nome": "Maria Cláudia Matos da Silva",
+      "email": "mariclaudia@gmail.com",
+      "tipoDeficiencia": "Visual",
+      "dataNascimento": {
+        "$date": "1999-12-20T00:00:00.000Z"
+      }
+    },
+    {
+      "nome": "Lucas Ferraz de Oliveira",
+      "email": "lucasferraz97@gmail.com",
+      "tipoDeficiencia": "Visual",
+      "dataNascimento": {
+        "$date": "2004-07-26T00:00:00.000Z"
+      }
+    }
+  ]
+
+
+
+    }
+  ]);
+  
